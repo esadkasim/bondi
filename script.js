d431e9953534565e48ee192d425363df2dee87d4
Esad kasım <111705020+esadkasim@users.noreply.github.com>
2024-01-04 14:17:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".auto-type",{
+    strings: [' zevkinizi bizimle keşfedin', 'en yeni moda ve fashion için istiyorsanız', 'doğru yerdesiniz'],
+    typeSpeed:150,
+    backSpeed:50,
+    loop:true
+})
+console.log('esad')
